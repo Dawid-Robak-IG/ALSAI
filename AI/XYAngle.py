4d8fe926706513eb65252f6643182e918f76fca0
dawid.robak <dawid.robak1704@gmail.com>
2025-09-16 22:54:59 +0200

    add scans paring

--- AI/XYAngle.py
+++ AI/XYAngle.py
@@ -9,9 +9,13 @@ from geometry_msgs.msg import PoseStamped
 import utilities
 
 import sys
+from colorama import Fore, Style, init
 
-delta_dist = 0.5
-delta_angle = 180.0
+init(autoreset=True)
+GREEN = "\033[32m"
+
+DELTA_DIST = 0.5
+DELTA_ANGLE = 180.0
 
 if len(sys.argv) < 2:
     rosbag_path = "../rosbags/map1_run2/map1_run2_0.db3"
@@ -46,5 +50,38 @@ while reader.has_next():
                                       msg.pose.position.y,
                                       yaw], dtype = np.float32)))
             
-print(f"Collected {len(scans)} scans and {len(poses)} positions.")
+print(Fore.GREEN + f"Collected {len(scans)} scans and {len(poses)} positions.")
 
+xya_scan_data = []
+scans_pars_data = []
+reallocates = []
+
+
+for t, scan in scans:
+    pose = utilities.get_pose_for_scan(t, poses)
+    xya_scan_data.append((scan,pose))
+
+for i in range(len(xya_scan_data)):
+    scan1,pose1 = xya_scan_data[i]
+    x1,y1,yaw1 = pose1
+
+    for j in range(i+1, len(xya_scan_data)):
+       scan2,pose2 = xya_scan_data[j]
+       x2,y2,yaw2 = pose2 
+
+       dx = x2 - x1
+       dy = y2 - y1
+       dist = np.sqrt(dx**2 + dy**2)
+
+       if dist <= DELTA_DIST:
+           delta_yaw = np.arctan2(np.sin(yaw2-yaw1),np.cos(yaw2-yaw1))
+           if abs(delta_yaw) <= DELTA_ANGLE:
+               scans_pars_data.append((scan1,scan2))
+               reallocates.append([dx,dy,delta_yaw])
+
+
+X = np.stack([np.stack([s1,s2], axis=-1) for s1,s2 in scans_pars_data])
+y = np.array(reallocates, dtype=np.float32)
+
+np.savez("dataset.npz", X=X,y=y)
+print(Fore.GREEN + "Dataset ready: ",X.shape, y.shape)
--- AI/utilities.py
+++ AI/utilities.py
@@ -3,3 +3,22 @@ import tf_transformations
 def quaternion_to_yaw(q):
     _, _, yaw = tf_transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])
     return yaw
+
+
+def get_pose_for_scan(scan_time, poses):
+    left, right = 0, len(poses) - 1
+    best_idx = 0
+    min_diff = abs(scan_time - poses[0][0])
+    
+    while left <= right:
+        mid = (left + right) // 2
+        diff = abs(scan_time - poses[mid][0])
+        if diff < min_diff:
+            min_diff = diff
+            best_idx = mid
+        if poses[mid][0] < scan_time:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return poses[best_idx][1]
