aa3bcdd2f1e71c020097ae950e929d3b350cf570
dawid.robak <dawid.robak1704@gmail.com>
2025-10-25 23:59:57 +0200

    add sh file to create data for all rosbags in diff files

---
+++ AI/create_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python3 create_data_rosbag.py map1_run1
+python3 create_data_rosbag.py map2_run1
+python3 create_data_rosbag.py map3_run1
+python3 create_data_rosbag.py map4_run1
+python3 create_data_rosbag.py map5_run1
+python3 create_data_rosbag.py map6_run1
+python3 create_data_rosbag.py map7_run1
