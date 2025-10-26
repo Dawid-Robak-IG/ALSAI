08c9d0fa5ffcd2025a95645f87b479908a2136e2
dawid.robak <dawid.robak1704@gmail.com>
2025-10-26 15:37:13 +0100

    add summary option

---
+++ AI/sum_models.sh
@@ -0,0 +1,6 @@
+python3 summary.py model_conv1
+python3 summary.py model_conv2
+python3 summary.py model_conv3
+python3 summary.py model_conv1_dropout
+python3 summary.py model_conv2_dropout
+python3 summary.py model_conv3_dropout
---
+++ AI/summary.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from colorama import Fore, init
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, Flatten, Dense, Dropout, Input, GlobalAveragePooling1D, LeakyReLU, BatchNormalization
+from tensorflow.keras import regularizers
+
+
+import build_model
+
+def model_sum(model_name):
+    init(autoreset=True)
+
+    print(Fore.GREEN + f"Summary for: {model_name}")
+    model = build_model.build_model(model_name)
+
+    model.build((None,360,2))
+    model.summary()
+
+def main():
+    if len(sys.argv) > 1:
+        model_name = sys.argv[1]
+    else:
+        print(Fore.YELLOW + "Didn't get name for model")
+        sys.exit(1)
+    model_sum(model_name)
+
+if __name__ == "__main__":
+    main()
